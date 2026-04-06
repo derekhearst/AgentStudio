0101a53ff599bd2a5d19101a6a07405134a89de0
Derek Hearst <derekallenhearst@gmail.com>
2026-04-05 20:26:25 -0600

    refactor(create-db): standardize code formatting and improve readability

--- scripts/create-db.ts
+++ scripts/create-db.ts
@@ -1,18 +1,18 @@
-import postgres from "postgres";
+import postgres from 'postgres'
 
 const sql = postgres({
-  host: "192.168.0.2",
-  port: 5432,
-  user: "derek",
-  password: "d8ACeA2x9oW#oP",
-  database: "postgres",
-});
+	host: '192.168.0.2',
+	port: 5432,
+	user: 'derek',
+	password: 'd8ACeA2x9oW#oP',
+	database: 'postgres',
+})
 
 try {
-  await sql`CREATE DATABASE "AGENTSTUDIO" TEMPLATE template0`;
-  console.log("Database AGENTSTUDIO created successfully.");
+	await sql`CREATE DATABASE "AGENTSTUDIO" TEMPLATE template0`
+	console.log('Database AGENTSTUDIO created successfully.')
 } catch (e: any) {
-  console.error("Error:", e.message);
+	console.error('Error:', e.message)
 } finally {
-  await sql.end();
+	await sql.end()
 }
